3b0b294cd4af45155fade75017b848cd0ebc39ab
aymenla <aymen.laouini@ymail.com>
2020-11-01 18:16:22 +0100

    adding a new course - OCR 5 - TP - ZFraction

---
+++ 5_TP_ZFraction/include/zfraction.hpp
@@ -0,0 +1,19 @@
+#ifndef _ZFRACTION_H_
+#define _ZFRACTION_H_
+
+#include <iostream>
+#include <string>
+
+class zfraction
+{
+
+    public:
+   
+    ~zfraction();
+
+    private:
+
+
+};
+
+#endif /* _ZFRACTION_H_ */
---
+++ 5_TP_ZFraction/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "zfraction.hpp"
+
+using namespace std;
+
+int main()
+{
+   
+    return 0;
+}
