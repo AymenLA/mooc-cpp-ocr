cc5ff3f1f0786e65625ef9acedccc4ed669d70ff
jeanlinux <aymen.laouini@ymail.com>
2021-04-25 17:42:30 +0200

    [MOOC-CPP] adding class ZFraction forlder and its compilation directories to cmakelist

---
+++ tp_zfraction/app/components/class_ZFraction/include/class_ZFraction.hpp
@@ -0,0 +1,36 @@
+#ifndef _ZFRACTION_HPP_
+#define _ZFRACTION_HPP_
+/**
+ * @file ZFraction.hpp
+ * @brief 
+ * @note 
+ *
+ * @copyright
+ *
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include <iostream>
+#include <cstdint>
+
+/* Public defines ------------------------------------------------------------*/
+
+/* Public declarations -------------------------------------------------------*/
+
+/* Public enums --------------------------------------------------------------*/
+
+/* Public structures ---------------------------------------------------------*/
+
+/* Public typedefs -----------------------------------------------------------*/
+
+/* Public functions ----------------------------------------------------------*/
+class ZFraction
+{
+    public:
+    ZFraction();
+    ~ZFraction();
+
+    private:
+};
+
+#endif /* _ZFRACTION_HPP_ */
