2bacc2947b1d0a78cc8c54e0707d781cbe7c06a5
jeanlinux <aymen.laouini@ymail.com>
2021-04-25 16:48:37 +0200

    [MOOC-CPP] adding files related to tp zfraction

---
+++ tp_zfraction/app/main/src/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file main.cpp
+ * @brief 
+ *
+ * @copyright
+ */
+/* Includes ------------------------------------------------------------------*/
+#include <iostream>
+#include <cstdint>
+
+
+#include "application_ecode.h"
+
+using namespace std;
+/* Private defines -----------------------------------------------------------*/
+
+/* Private declarations ------------------------------------------------------*/
+
+/* Private enums -------------------------------------------------------------*/
+
+/* Private structures --------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+
+/* Public functions ----------------------------------------------------------*/
+int32_t main(int32_t argc, char ** argv)
+{
+    int32_t ret_value = APP_ECODE_OK;
+
+    return ret_value;
+}
